6073147941593cd72c21d39fbc83719b1c61bade
Sagi Dana <sagidana1@gmail.com>
2024-07-11 23:03:38 +0300

    adding actions: view, select, create

--- knowit.py
+++ knowit.py
@@ -1,19 +1,123 @@
-from os import walk, path
+from subprocess import Popen, PIPE, DEVNULL
+from datetime import datetime
+from sys import stdin, stdout, stderr
+from os import walk, path, environ
+import traceback
+import argparse
 
 from note import Note
 
 
+def gotovim(path, content):
+    try:
+        cmd = ["nvim", path, "-c", f"normal i{content}"]
+        env = environ.copy()
+        p = Popen(cmd,
+                  stdin=stdin,
+                  stdout=stdout,
+                  env=env)
+        output, errors = p.communicate()
+    except Exception as e: print(f"traceback: {traceback.format_exc()}")
+
+def tags_selection_preview():
+    import sys
+    from subprocess import run
+    if len(sys.argv) < 2: return
+    tags = sys.argv[1:]
+
+    cmd = ["python3",
+           "/home/s/github/knowit/knowit.py",
+           "-a", "view",
+           "-t"]
+    cmd.extend(tags)
+    result = run(cmd, capture_output=True, text=True)
+    print(result.stdout)
+
+def fzf(options, preview_cb):
+    import inspect
+    """
+    This is so cool, fzf print out to stderr the fuzzing options,
+    and only the chosen result spit to the stdout.. this enables scripts like
+    this to work out of the box, no redirection of the stderr is need - and
+    only the result is redirected to our pipe (which contain the result)
+    FZF - good job :)
+    NOTE: influenced by https://jeskin.net/blog/grep-fzf-clp/
+    NOTE: https://github.com/jpe90/clp is needed to be installed!
+    """
+    try:
+        source = inspect.getsource(preview_cb)
+        source += "\n"
+        source += "from inspect import getmembers, isfunction\n"
+        source += "from sys import modules\n"
+        source += "\n"
+        source += "for _,obj in getmembers(modules[__name__]):\n"
+        source += "    if not isfunction(obj): continue\n"
+        source += "    if not isfunction(obj): continue\n"
+        source += "    if 'preview' not in obj.__name__: continue\n"
+        source += "    obj()\n"
+        open("/tmp/preview.py", "w+").write(source)
+
+        fzf_options = "--bind 'ctrl-z:toggle-preview' "
+        fzf_options += "--tiebreak=index "
+        # fzf_options += "--preview-window '90%' "
+        fzf_options += "--preview-window 'up,80%' "
+        fzf_options += "--multi " # mutli selection of options
+        # fzf_options += "--preview 'python /tmp/preview.py {+} | bat -l md --style=full --color=always'"
+        fzf_options += "--preview 'python /tmp/preview.py {+}'"
+
+        env = environ.copy()
+        env["FZF_DEFAULT_OPTS"] = fzf_options
+        p = Popen(["fzf"],
+                  stdin=PIPE,
+                  stdout=PIPE,
+                  stderr=stderr,
+                  env=env)
+        # write the options to stdin before launching the pocess with communicate()
+        p.stdin.write("\n".join(options).encode())
+
+        output, errors = p.communicate()
+
+        results = output.decode('utf-8').strip()
+        return results.splitlines()
+    except Exception as e: print(f"traceback: {traceback.format_exc()}")
+
+def bat(content):
+    """
+    Calling bat for syntax highlighting
+    """
+    try:
+        env = environ.copy()
+        p = Popen(["bat",
+                   "-l", "md", # markdown language
+                   "--style=auto",
+                   "--color=always",
+                   ],
+                  stdin=PIPE,
+                  stdout=PIPE,
+                  stderr=stderr,
+                  env=env)
+        # write the options to stdin before launching the pocess with communicate()
+        p.stdin.write(content.encode())
+        output, errors = p.communicate()
+        return output
+
+    except Exception as e: print(f"traceback: {traceback.format_exc()}")
+
 class Knowit():
-    def __init__(self):
-        self.cwd = "~/notes"
+    def __init__(self, args=None):
+        self.cwd = path.expanduser("~/notes")
         self.notes = self.parse_notes()
+        self.args = args
 
     def parse_notes(self):
         notes = []
-        for root, dir, files in walk(path.expanduser(self.cwd)):
+        for root, dir, files in walk(self.cwd):
             for f in files:
                 file_path = path.join(root, f)
-                notes.append(Note(file_path))
+                try:
+                    note = Note.parse(file_path)
+                    notes.append(note)
+                except: continue
         return notes
 
     def get_tags(self):
@@ -25,14 +129,76 @@ class Knowit():
 
         return all_tags
 
+    def get_links(self):
+        all_links = []
+
+        for note in self.notes:
+            for link in note.links:
+                # link_from = link[0]
+                link_id = link[1]
+                # link_to = link[2]
+                all_links.append(link_id)
+        all_links = list(set(all_links))
+        return all_links
+
+    def create_note(self):
+        i = 0
+        while path.exists(path.join(self.cwd, f"{i}.md")): i += 1
+        note_path = path.join(self.cwd, f"{i}.md")
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        content = f"[{timestamp}]\n"
+        content += "="*80 + "\n"
+
+        gotovim(note_path, content)
+
+    def select_tags(self):
+        tags = self.get_tags()
+        selected_tags = fzf(tags, tags_selection_preview)
+
+        # TODO: create vim view with folds and markdown
+        print(f"selected_tags: {selected_tags}")
+
+    def view(self):
+        if len(self.args.tags) == 0: return
+
+        content = ""
+        for note in self.notes:
+            if not set(self.args.tags).issubset(set(note.tags)): continue
+            content += ''.join(note.content)
+        stdout.buffer.write(bat(content))
+
 
 def main():
-    try:
-        knowit = Knowit()
-        print(knowit.get_tags())
-    except:
-        import traceback
-        print(traceback.format_exc())
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-a',
+                        '--action',
+                        choices=[
+                                    "create",
+                                    "select",
+                                    "view",
+                                ],
+                        help="the action to be perfomed")
+    parser.add_argument('-t',
+                        '--tags',
+                        nargs='+',
+                        default=[],
+                        help="list of tags to perform action on.")
+
+    args = parser.parse_args()
+    knowit = Knowit(args)
+
+    if args.action == "create":
+        knowit.create_note()
+    if args.action == "select":
+        knowit.select_tags()
+    if args.action == "view":
+        knowit.view()
+
+    # knowit.notes[0].dump()
+    # print(knowit.get_tags())
 
 if __name__=="__main__":
-    main()
+    try:
+        main()
+    except:
+        print(traceback.format_exc())
--- note.py
+++ note.py
@@ -3,18 +3,50 @@ import re
 
 
 class Note():
-    def __init__(self, path):
+    def __init__(self,
+                 path,
+                 timestamp,
+                 tags,
+                 links,
+                 content):
+        self.path = path
+        self.timestamp = timestamp
+        self.tags = tags
+        self.links = links
+        self.content = content
+
+    def dump(self):
+        timestamp_str = self.timestamp.strftime("%Y-%m-%d %H:%M:%S")
+        to_dump = f"[{timestamp_str}]"
+        for tag in self.tags: to_dump += f" #{tag}"
+        to_dump += "\n"
+        to_dump += "="*80 + "\n"
+        to_dump += "".join(self.content)
+        open(self.path, 'w').write(to_dump)
+
+    @staticmethod
+    def parse(path):
         lines = open(path, 'r').readlines()
         assert len(lines) > 2
 
-        m = re.match(r"^\[(?P<date>\d\d\d\d-\d\d\-\d\d\ \d\d:\d\d:\d\d)\]\s(?P<tags>(\#\w+\s)*)?$", lines[0])
+        m = re.match(r"^\[(?P<date>\d\d\d\d-\d\d\-\d\d\ \d\d:\d\d:\d\d)\]\s+(?P<tags>(\#\w+\s+)*)?$", lines[0])
         assert m is not None
 
-        self.timestamp = datetime.strptime(m.group('date'), "%Y-%m-%d %H:%M:%S")
-        self.tags = m.group('tags').strip().split('#')[1:]
+        timestamp = datetime.strptime(m.group('date'), "%Y-%m-%d %H:%M:%S")
+        tags = m.group('tags').strip().split('#')[1:]
+        tags = [tag.strip() for tag in tags]
 
         m = re.match(r"^={80}$", lines[1])
         assert m is not None
 
-        self.note = lines[2:]
-        self.links = []
+        content = lines[2:]
+        links = []
+        for line in content:
+            m = re.match(r"^.*\[(?P<from>=>)?(?P<link>\w{10})(?P<to>=>)?\].*$", line)
+            if m is None: continue
+            link_id = m.group('link')
+            link_from = m.group('from')
+            link_to = m.group('to')
+            links.append((link_from is not None, link_id, link_to is not None))
+
+        return Note(path, timestamp, tags, links, content)
